453d3698cce90f92c248efff9c4b11ea32016b9b
xkakus <xkakus@gmail.com>
2023-10-04 11:25:26 -0400

    Files will be grouped with a subject, if subject is None, skip and move to regular

--- Python/file_sorter_v2.py
+++ Python/file_sorter_v2.py
@@ -1,33 +1,37 @@
-import os, shutil
+import os, shutil, re
 
-def rename_if_exists(destination, extension, filename): # gets the target destination, then the filename (file.txt)
-    base, ext = os.path.splitext(filename)   # next, the file is split into two parts by the dot
-    i = 1                                    # this creates and assings a variable
-    while True:                              # conditional test
-        new_filename = f"{base} ({i}){ext}"  # a template string, it will use the splitted file, base the front, i, the incriment, and ext, the file extension
-        new_path = os.path.join(destination, extension, new_filename) # this creates a new variable, this will create the file path (User/user/Desktop/folder/new_filename (1).txt)
-        if not os.path.exists(new_path):     # this tests if the name is already taken, if not, return the new path, then incriment again to prevent duplicate of duplicate file name
+def rename_if_exists(destination, extension, filename):
+    base, ext = os.path.splitext(filename)
+    i = 1
+    while True:
+        new_filename = f"{base} ({i}){ext}"
+        new_path = os.path.join(destination, extension, new_filename)
+        if not os.path.exists(new_path):
             return new_path
         i += 1
 
-path = input('What is the target folder: ')  # this prompts for the target folder path, and assigns a value
+# Function to get info from file
+def get_info(fileName):
+    fileName = fileName.split(r'_')
+    fileName[1] = fileName[1].lower()
+    if fileName[1] in ['acct', 'econ', 'mis', 'stat']:
+        return fileName[1]
+    else:
+        return None
+
+path = input('What is the target folder: ')
 if path == '':
-    path = 'C:/Users/yuxua/Desktop/test'
+    path = '/home/xkaku/Desktop/test'
 
-files = os.listdir(path)                     # this will list the files within the target directory
+files = os.listdir(path)
 
-# for each file in the directory
 for file in files:
-    # this creates the complete file path for each file in the directory
     file_path = os.path.join(path, file)
 
-    if os.path.isfile(file_path):  # Check if it's a file
-        # this splits the file into two part, the name of the file, and the extension
+    if os.path.isfile(file_path):
         fileName, ext = os.path.splitext(file)
-        # this gets rid of the period
         ext = ext[1:]
 
-        # this compares the extension, if matched, then assign the variable destination to the target path
         if ext in ["gif", "jpg", "png", "jpeg", "bmp", "tif", "tiff"]:
             destination = os.path.join(path, 'images')
         elif ext in ['doc', 'docx', 'xls', 'xlsx', 'pdf', 'txt', 'ai', 'svg', 'eps', 'idml', 'indd', 'pptx', 'ppt', 'xopp']:
@@ -39,22 +43,23 @@ for file in files:
         else:
             destination = os.path.join(path, 'random', ext)
                 
-        # if the directory doesnt exists, then make it
         if not os.path.exists(destination):
             os.makedirs(destination)
 
-        # assign the new file path
-        if not os.path.exists(os.path.join(destination, ext)):
-            os.makedirs(os.path.join(destination, ext))
+        subject = get_info(fileName)
+        if subject == None:
+            if not os.path.exists(os.path.join(destination, ext)):
+                os.makedirs(os.path.join(destination, ext))
+            new_path = os.path.join(destination, ext, file)
+            if os.path.exists(new_path):
+                new_path = rename_if_exists(destination, ext, file)
+        else:
+            if not os.path.exists(os.path.join(destination, subject, ext)):
+                os.makedirs(os.path.join(destination, subject, ext))
+            new_path = os.path.join(destination, subject, ext, file)
+            if os.path.exists(new_path):
+                new_path = rename_if_exists(destination, subject, ext, file)
 
-        new_path = os.path.join(destination, ext, file)
-
-        # tests for repeated name in the targeted path, if it does, call the function rename_if_exists() and rename the file
-        if os.path.exists(new_path):
-            new_path = rename_if_exists(destination, ext, file)
-
-        # moves the file to the target directory
         shutil.move(file_path, new_path)
     else:
-        # ignored directory
         print(f"Skipping directory: {file_path}")
